bb856f00edded63e96ad81b852bb7713b9b50e3a
zai-i <zainab@wonderbly.com>
2023-07-12 14:22:40 +0100

    Fix

--- api/index.js
+++ api/index.js
@@ -3,7 +3,6 @@ const app = express();
 const port = 3000;
 const cors = require('cors');
 const sonnets = require('./sonnets.js');
-const syl = require('syllabificate');
 
 var corsOptions = {
   origin: [
